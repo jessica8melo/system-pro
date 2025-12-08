186adb41c6c953bcb0101c3b34ecd8d4a4a0cbfd
anapaulaonc <anapaulaoncosta@gmail.com>
2025-12-08 10:25:14 +0100

    merging

--- enseash.c
+++ enseash.c
@@ -1,11 +1,28 @@
 #include "question1.h"
 
+
+//unecessary function
+void print_random_fortune() {
+    const char* fortunes[] = {
+        "You will pass power electronics.\n",
+        "You will nail power electronics.\n",
+        "Study more for electronics.\n"
+    };
+    int count = 3;
+    srand(time(NULL)); // Seed with timestamp
+    int idx = rand() % count; // Random index to the fortune
+    write(STDOUT_FILENO, fortunes[idx], strlen(fortunes[idx]));
+}
+
+
+
+
 int read_command(char* buffer) {
     write(STDOUT_FILENO, WELCOME_MSG, strlen(WELCOME_MSG)); // mensagem bemvindoos
     write(STDOUT_FILENO, PROMPT, strlen(PROMPT)); 
-    ssize_t bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1); // 
+    ssize_t bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1); // leave space for enter
     if (bytes <= 0){
-        return -1;
+        return -1; // EOF or error
     } 
     buffer[bytes] = '\0';
     char* nl = strchr(buffer, '\n');
@@ -18,17 +35,25 @@ int read_command(char* buffer) {
 
 void execute_command(const char* cmd) {
     if (strcmp(cmd, "exit") == 0) {
+        write(STDOUT_FILENO, GOODBYE_MSG, strlen(GOODBYE_MSG));
         exit(0); 
     }
+    if (strcmp(cmd, "elctronics-fortune") == 0) {
+        print_random_fortune();
+        return;
+    }
+
 
     pid_t pid = fork();
     if (pid == 0) {
-        execlp(cmd, cmd, (char*)NULL);
+        execlp(cmd, cmd, NULL); 
         exit(1);
     } else if (pid > 0) {
         wait(NULL);
     }
 }
+
+
 
 int main() {
     char buffer[BUFFER_SIZE];
--- question1.h
+++ question1.h
@@ -6,14 +6,25 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h> // time for seeding rand
+
 
 #define PROMPT "enseash % "
 #define BUFFER_SIZE 1024
 
+#define WELCOME_MSG "Bem vindo a o shell da ENSEA!!!!!!!\n Pra quitar, escreva: 'exit'\n"
+
+#define GOODBYE_MSG "Beijos, tchau!\n"
+
 int read_command(char* buffer);
 void execute_command(const char* cmd);
+void print_random_fortune();
 
 
-#define WELCOME_MSG "Bem vindo a o shell da ENSEA!!!!!!!\n Pra quitar, escreva: 'exit'.\n"
+
+
+
+
+
 
 #endif
